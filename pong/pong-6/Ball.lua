27b5e2c9b99bfd16e6189f1e79baa35fed5acdb5
Sajith Madhusankha <smadhusankha0@gmail.com>
2023-06-20 22:27:12 +0530

    Add reset, update, render classes

--- pong/pong-6/Ball.lua
+++ pong/pong-6/Ball.lua
@@ -42,3 +42,26 @@ function Ball:collides(paddle)
     -- if the above aren't true, they're overlapping
     return true
 end
+
+--[[ 
+    Places the ball in the middle of the screen, with an initial random velocity 
+    on both axes.
+]]
+function Ball:reset()
+    self.x = VIRTUAL_WIDTH / 2 - 2
+    self.y = VIRTUAL_HEIGHT / 2 - 2
+    self.dy = math.random(2) == 1 and 100 or -100
+    self.dx = math.random(-50, 50)
+end
+
+--[[
+    Simply applies velocity to position, scaled by deltaTime.
+]]
+function Ball:update(dt)
+    self.x = self.x + self.dx * dt
+    self.y = self.y + self.dy * dt
+end
+
+function Ball:render()
+    love.graphics.rectangle('fill', self.x, self.y, self.width, self.height)
+end
