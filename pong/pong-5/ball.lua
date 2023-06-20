d5088099acbdaca50a413e2925e35db05ccc6fae
Sajith Madhusankha <smadhusankha0@gmail.com>
2023-06-20 16:33:40 +0530

    Add Ball.lua

---
+++ pong/pong-5/ball.lua
@@ -0,0 +1,46 @@
+--[[
+    Pong Remake
+
+    --Ball Class--
+
+    Represents a ball which will bounce back and forth between paddles 
+    and walls until it passes a left or right boundary of the screen, 
+    scoring a point for
+]]
+
+Ball = class{}
+
+function Ball:init(x, y, width, height)
+    self.x = x
+    self.y = y
+    self.width = width
+    self.height = height
+
+    -- these variables are for keeping track of our velocity on both the
+    -- X and Y axis, since the ball can move in two dimensions
+    self.dx = math.random(2) == 1 and 100 or -100
+    self.dy = math.random(-50, 50)
+end
+
+--[[
+    Places the ball in the middle of the screen, with an initial random velocity
+    on both axes.
+]]
+function Ball:reset()
+    self.x = VIRTUAL_WIDTH / 2 - 2
+    self.y = VIRTUAL_HEIGHT / 2 - 2
+    self.dx = math.random(2) == 1 and 100 or -100
+    self.dy = math.random(-50, 50)
+end
+
+--[[
+    Simply applies velocity to position, scaled by deltaTime.
+]]
+function Ball:update(dt)
+    self.x = self.x + self.dx * dt
+    self.y = self.y + self.dy * dt
+end
+
+function Ball:render()
+    love.graphics.rectangle('fill', self.x, self.y, self.width, self.height)
+end
