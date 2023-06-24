45399b9a1e77b20e87a2169452d33a407648b8e1
Sajith Madhusankha <smadhusankha0@gmail.com>
2023-06-24 10:20:44 +0530

    add day 4 update with jump option

---
+++ fifty-bird/bird4/Bird.lua
@@ -0,0 +1,31 @@
+Bird = Class{}
+
+local GRAVITY = 20
+
+function Bird:init()
+    self.image = love.graphics.newImage('bird.png')
+    self.width = self.image:getWidth()
+    self.height = self.image:getHeight()
+
+    self.x = VIRTUAL_WIDTH / 2 - (self.width / 2)
+    self.y = VIRTUAL_HEIGHT / 2 - (self.height / 2)
+
+    self.dy = 0
+end
+
+function Bird:update(dt)
+    -- apply gravity to velocity
+    self.dy = self.dy + GRAVITY * dt
+
+    -- add a sudden burst of negative gravity if we hit space
+    if love.keyboard.wasPressed('space') then
+        self.dy = -5
+    end
+
+    -- apply current velocity to Y position
+    self.y = self.y + self.dy
+end
+
+function Bird:render()
+    love.graphics.draw(self.image, self.x, self.y)
+end
