ce83f7b81c4f8d1aa98d4a43116112c16a61189f
Sajith Madhusankha <smadhusankha0@gmail.com>
2023-06-24 17:51:29 +0530

    add bird.lua

---
+++ fifty-bird/Bird.lua
@@ -0,0 +1,30 @@
+Bird = Class{}
+
+local GRAVITY = 20
+
+function Bird:init()
+    self.image = love.graphics.newImage('bird.png')
+    self.width = self.image:getWidth()
+    self.height = self.image:getHeight()
+
+    -- position bird in the middle of the screen
+    self.x = VIRTUAL_WIDTH / 2 - (self.width / 2)
+    self.y = VIRTUAL_HEIGHT / 2 - (self.height / 2)
+
+    -- velocity of the bird
+    self.dy = 0
+end
+
+function Bird:update(dt)
+    self.dy = self.dy + GRAVITY * dt
+
+    if love.keyboard.wasPressed('space') then
+        self.dy = -5
+    end
+
+    self.y = self.y + self.dy
+end
+
+function Bird:render()
+    love.graphics.draw(self.image, self.x, self.y)
+end
