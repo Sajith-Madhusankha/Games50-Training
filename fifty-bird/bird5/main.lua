9368807dc877851917860c2f16a8cbb9d76d327f
Sajith Madhusankha <smadhusankha0@gmail.com>
2023-06-24 17:57:10 +0530

    Add Day 5 update with pipe.lua

---
+++ fifty-bird/bird5/main.lua
@@ -0,0 +1,114 @@
+--[[
+    Flappy Bird Remake
+
+    bird5
+    --The infinite Pipe Update--
+]]
+
+push = require 'push'
+
+Class = require 'class'
+
+require 'Bird'
+
+require 'Pipe'
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+VIRTUAL_WIDTH = 512
+VIRTUAL_HEIGHT = 288
+
+local background = love.graphics.newImage('background.png')
+local backgroundScroll = 0
+
+local ground = love.graphics.newImage('ground.png')
+local groundScroll = 0
+
+local BACKGROUND_SCROLL_SPEED = 30
+local GROUND_SCROLL_SPEED = 60
+
+local BACKGROUND_LOOPING_POINT = 413
+
+local bird = Bird()
+
+local pipes = {}
+
+local spawnTimer = 0
+
+function love.load()
+
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    love.window.setTitle('Fifty Bird')
+
+    math.randomseed(os.time())
+
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        
+        fullscreen = false,
+        resizable = true,
+        vsync = true
+    })
+
+    love.keyboard.keysPressed = {}
+end
+
+function love.resize(w, h)
+    push:resize(w, h)
+end
+
+function love.keypressed(key)
+
+    love.keyboard.keysPressed[key] = true
+
+    if key == 'escape' then
+        love.event.quit()
+    end
+end
+
+function love.keyboard.wasPressed(key)
+
+    return love.keyboard.keysPressed[key]
+end
+
+function love.update(dt)
+
+    backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) 
+        % BACKGROUND_LOOPING_POINT
+
+    groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) 
+        % VIRTUAL_WIDTH
+
+    spawnTimer = spawnTimer + dt
+
+    if spawnTimer > 2 then
+        table.insert(pipes, Pipe())
+        spawnTimer = 0
+    end
+
+    bird:update(dt)
+
+    for k, pipe in pairs(pipes) do
+        pipe:update(dt)
+    end
+
+    love.keyboard.keysPressed = {}
+end
+
+function love.draw()
+
+    push:start()
+
+    love.graphics.draw(background, -backgroundScroll, 0)
+
+    for k, pipe in pairs(pipes) do
+        pipe:render()
+    end
+
+    love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
+
+    bird:render()
+
+    push:finish()
+end
